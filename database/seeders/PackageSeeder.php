5a4cb82c369027a65b237b19f472232b88d71f89
Odense <thoriqalhakim01@gmail.com>
2025-06-22 21:32:21 +0700

    feat: add database seeder for package seeder

---
+++ database/seeders/PackageSeeder.php
@@ -0,0 +1,39 @@
+<?php
+namespace Database\Seeders;
+
+use App\Models\Package;
+use Illuminate\Database\Seeder;
+
+class PackageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $packages = [
+            [
+                'name'     => 'Pay/visit',
+                'price'    => 0,
+                'points'   => 0,
+                'duration' => 0,
+            ],
+            [
+                'name'     => 'Gold',
+                'price'    => 2000000,
+                'points'   => 50,
+                'duration' => 180,
+            ],
+            [
+                'name'     => 'Silver',
+                'price'    => 1800000,
+                'points'   => 40,
+                'duration' => 180,
+            ],
+        ];
+
+        foreach ($packages as $package) {
+            Package::create($package);
+        }
+    }
+}
