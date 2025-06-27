27ef09a909735433fd5c3540f34a9a154751b9ed
Odense <thoriqalhakim01@gmail.com>
2025-06-27 19:54:52 +0700

    feat: attendance seeder

--- database/seeders/DummySeeder.php
+++ database/seeders/DummySeeder.php
@@ -1,9 +1,13 @@
 <?php
 namespace Database\Seeders;
 
+use App\Models\Attendance;
 use App\Models\Member;
 use App\Models\Package;
 use App\Models\Point;
+use App\Models\Trainer;
+use App\Models\TrainingSession;
+use App\Models\TrainingSessionMember;
 use App\Models\Transaction;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -105,6 +109,156 @@ class DummySeeder extends Seeder
                     'payment_method'   => $paymentMethod,
                     'notes'            => 'Transaction for ' . $member->name . ' - ' . $selectedPackage->name . ' package',
                 ]);
+            }
+        }
+
+        $allTrainers = Trainer::all();
+
+        $memberAttendanceCount  = [];
+        $trainerAttendanceCount = [];
+
+        foreach ($allMembers as $member) {
+            $memberAttendanceCount[$member->id] = 0;
+        }
+
+        foreach ($allTrainers as $trainer) {
+            $trainerAttendanceCount[$trainer->id] = 0;
+        }
+
+        $totalSessions = 100;
+
+        for ($i = 0; $i < $totalSessions; $i++) {
+            $selectedTrainer = $allTrainers->random();
+
+            $sessionTimestamp = now()->subDays(rand(1, 180))
+                ->setTime(rand(6, 22), rand(0, 59), rand(0, 59));
+
+            $trainingSession = TrainingSession::create([
+                'staff_id'        => $admin->id,
+                'trainer_id'      => $selectedTrainer->id,
+                'entry_timestamp' => $sessionTimestamp,
+            ]);
+
+            Attendance::create([
+                'staff_id'        => $admin->id,
+                'attendable_id'   => $selectedTrainer->id,
+                'attendable_type' => Trainer::class,
+                'entry_timestamp' => $sessionTimestamp,
+            ]);
+
+            $trainerAttendanceCount[$selectedTrainer->id]++;
+
+            $sessionMemberCount = rand(1, 5);
+
+            $availableMembers = $allMembers->filter(function ($member) use ($memberAttendanceCount) {
+                return $memberAttendanceCount[$member->id] < 10;
+            });
+
+            if ($availableMembers->count() < $sessionMemberCount) {
+                $availableMembers = $allMembers;
+            }
+
+            $selectedMembers = $availableMembers->random(min($sessionMemberCount, $availableMembers->count()));
+
+            foreach ($selectedMembers as $member) {
+                TrainingSessionMember::create([
+                    'training_session_id' => $trainingSession->id,
+                    'member_id'           => $member->id,
+                ]);
+
+                Attendance::create([
+                    'staff_id'        => $admin->id,
+                    'attendable_id'   => $member->id,
+                    'attendable_type' => Member::class,
+                    'entry_timestamp' => $sessionTimestamp->copy()->addMinutes(rand(0, 10)),
+                ]);
+
+                $memberAttendanceCount[$member->id]++;
+            }
+        }
+
+        foreach ($allMembers as $member) {
+            $currentCount = $memberAttendanceCount[$member->id];
+
+            if ($currentCount < 10) {
+                $needed = 10 - $currentCount;
+
+                for ($j = 0; $j < $needed; $j++) {
+                    $selectedTrainer = $allTrainers->random();
+
+                    $sessionTimestamp = now()->subDays(rand(1, 180))
+                        ->setTime(rand(6, 22), rand(0, 59), rand(0, 59));
+
+                    $trainingSession = TrainingSession::create([
+                        'staff_id'        => $admin->id,
+                        'trainer_id'      => $selectedTrainer->id,
+                        'entry_timestamp' => $sessionTimestamp,
+                    ]);
+
+                    if ($trainerAttendanceCount[$selectedTrainer->id] < 10) {
+                        Attendance::create([
+                            'staff_id'        => $admin->id,
+                            'attendable_id'   => $selectedTrainer->id,
+                            'attendable_type' => Trainer::class,
+                            'entry_timestamp' => $sessionTimestamp,
+                        ]);
+                        $trainerAttendanceCount[$selectedTrainer->id]++;
+                    }
+
+                    TrainingSessionMember::create([
+                        'training_session_id' => $trainingSession->id,
+                        'member_id'           => $member->id,
+                    ]);
+
+                    Attendance::create([
+                        'staff_id'        => $admin->id,
+                        'attendable_id'   => $member->id,
+                        'attendable_type' => Member::class,
+                        'entry_timestamp' => $sessionTimestamp->copy()->addMinutes(rand(0, 10)),
+                    ]);
+                }
+            }
+        }
+
+        foreach ($allTrainers as $trainer) {
+            $currentCount = $trainerAttendanceCount[$trainer->id];
+
+            if ($currentCount < 10) {
+                $needed = 10 - $currentCount;
+
+                for ($j = 0; $j < $needed; $j++) {
+                    $sessionTimestamp = now()->subDays(rand(1, 180))
+                        ->setTime(rand(6, 22), rand(0, 59), rand(0, 59));
+
+                    $trainingSession = TrainingSession::create([
+                        'staff_id'        => $admin->id,
+                        'trainer_id'      => $trainer->id,
+                        'entry_timestamp' => $sessionTimestamp,
+                    ]);
+
+                    Attendance::create([
+                        'staff_id'        => $admin->id,
+                        'attendable_id'   => $trainer->id,
+                        'attendable_type' => Trainer::class,
+                        'entry_timestamp' => $sessionTimestamp,
+                    ]);
+
+                    $sessionMembers = $allMembers->random(rand(1, 3));
+
+                    foreach ($sessionMembers as $member) {
+                        TrainingSessionMember::create([
+                            'training_session_id' => $trainingSession->id,
+                            'member_id'           => $member->id,
+                        ]);
+
+                        Attendance::create([
+                            'staff_id'        => $admin->id,
+                            'attendable_id'   => $member->id,
+                            'attendable_type' => Member::class,
+                            'entry_timestamp' => $sessionTimestamp->copy()->addMinutes(rand(0, 10)),
+                        ]);
+                    }
+                }
             }
         }
     }
