bae110e6fce2773aa113c089e6fc3363eb9484b7
Odense <thoriqalhakim01@gmail.com>
2025-06-28 00:14:30 +0700

    feat: styling check in page

--- resources/js/layouts/client-layout.tsx
+++ resources/js/layouts/client-layout.tsx
@@ -1,15 +1,23 @@
+import { Separator } from '@/components/ui/separator';
 import { PropsWithChildren } from 'react';
 import ClientHeaderLayout from './client/client-header-layout';
 
 export default function ClientLayout({ children }: PropsWithChildren) {
     return (
-        <div className="light flex h-screen w-full flex-1 flex-col bg-background">
-            <div className="h-24z mx-auto w-full max-w-7xl px-4 lg:h-24 lg:px-0">
-                <ClientHeaderLayout />
+        <div className="relative flex h-screen w-full flex-1 flex-col">
+            <div className="absolute top-0 right-0 left-0">
+                <div className="mx-auto w-full max-w-5xl">
+                    <ClientHeaderLayout />
+                </div>
+                <Separator />
             </div>
-            <div className="mx-auto flex h-full w-full max-w-7xl flex-col">
-                <main className="my-6 flex flex-1 flex-col space-y-8">{children}</main>
-            </div>
+            <main className="mx-auto flex min-h-full w-full max-w-5xl flex-1 flex-col items-center justify-center">
+                <div className="flex w-full flex-1 flex-col items-center justify-center gap-6">
+                    <img src="/logo.png" className="h-36 w-36" alt="Logo" />
+                    <h1 className="text-center font-serif text-6xl text-balance">Make Your Body Healthy And Physical Feat.</h1>
+                    {children}
+                </div>
+            </main>
         </div>
     );
 }
